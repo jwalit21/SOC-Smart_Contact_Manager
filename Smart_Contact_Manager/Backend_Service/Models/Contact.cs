90c549d295fb7d203e41e414733e4a029343d804
jwalit21 <jwalitshah2q@gmail.com>
2021-01-27 23:53:48 +0530

    Account service completed + Service Host added

---
+++ Smart_Contact_Manager/Backend_Service/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend_Service.Models
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Description { get; set; }
+        public string PhotoPath { get; set; }
+    }
+}
---
+++ Smart_Contact_Manager/Backend_Service/Models/Group.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend_Service.Models
+{
+    public class Group
+    {
+        public Group()
+        {
+        }
+        public int GroupId { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
