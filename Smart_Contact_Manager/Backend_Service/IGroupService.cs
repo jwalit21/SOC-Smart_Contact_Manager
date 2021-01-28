0697c379530856d3b6dadfe6b7bdfdb9328d8ea6
ayanshaikh18 <mahammadayan18@gmail.com>
2021-01-28 16:22:20 +0530

    Group Service implemented

---
+++ Smart_Contact_Manager/Backend_Service/IGroupService.cs
@@ -0,0 +1,39 @@
+﻿using Backend_Service.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Backend_Service
+{
+    [ServiceContract]
+    public interface IGroupService
+    {
+        [OperationContract]
+        List<Group> GetGroups(int userId);
+
+        [OperationContract]
+        Group AddGroup(Group group);
+
+        [OperationContract]
+        Group GetGroup(Group group);
+
+        [OperationContract]
+        Group UpdateGroup(Group group);
+
+        [OperationContract]
+        Group DeleteGroup(Group group);
+
+        [OperationContract]
+        GroupContact AddGroupContract(GroupContact groupContract);
+
+        [OperationContract]
+        List<GroupContact> GetGroupContacts(int GroupId);
+
+        [OperationContract]
+        GroupContact DeleteGroupContact(GroupContact groupContact);
+
+    }
+}
